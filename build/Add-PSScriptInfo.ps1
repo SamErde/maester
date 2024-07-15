2fbd694ba1bc74d7c5211c01c3c628eb0b89bba5
Sam Erde <samuel.erde@gmail.com>
2024-07-15 12:38:16 -0400

    Added required fields for PSScriptInfo

--- build/Add-PSScriptInfo.ps1
+++ build/Add-PSScriptInfo.ps1
@@ -3,20 +3,27 @@ $TestFiles = gci *.ps1 -Recurse
 foreach ($file in $TestFiles) {
 
     $FilePath = $file.FullName
-    $Directory = $file.Directory
-    $Description = "Maester Test _____"
+    $DirectoryName = $($file.DirectoryName).Split('\')[-1]
+    $Description = "Maester Test: $($file.Name)"
     $Author = "Maester Team"
-    $Tags = "active CISA $Directory"
+    switch -regex ($FilePath) {
+        [eidsca] { $Category = 'EIDSCA'}
+        [CISA] { $Category = 'CISA' }
+        default { $Category = 'Core' }
+    }
+    $Status = 'Active'
+    $TagSort = @("$Category, $DirectoryName") | Sort-Object -Unique
+    $Tags = "$Status, $($TagSort.ToString())"
     $Version = '0.0.1'
 
     $Info = @"
 <#PSScriptInfo
 .DESCRIPTION $Description
 .VERSION $Version
-.AUTHOR = $Author
+.AUTHOR $Author
 .TAGS $Tags
 #>
-
+`n
 "@
 
     $FileContent = Get-Content -Path $file
