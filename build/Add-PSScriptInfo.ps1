a1327d3a3f2c00b1b2987ce9726b9b180f3959f4
Sam Erde <samuel.erde@gmail.com>
2024-07-20 14:56:22 -0400

    Complete, valid PSScriptInfo blocks

--- build/Add-PSScriptInfo.ps1
+++ build/Add-PSScriptInfo.ps1
@@ -1,33 +1,36 @@
-$TestFiles = gci *.ps1 -Recurse
+$BasePath = Join-Path -Path $PSScriptRoot "..\powershell"
+Set-Location -Path $BasePath
+$TestFiles = Get-ChildItem "Test-Mt*.ps1" -Recurse -Exclude "*.Tests.ps1"
 
 foreach ($file in $TestFiles) {
 
-    $FilePath = $file.FullName
-    $DirectoryName = $($file.DirectoryName).Split('\')[-1]
+    $FilePath = "$($file.FullName)"
     $Description = "Maester Test: $($file.Name)"
-    $Author = "Maester Team"
+    $Guid = (New-Guid)
     switch -regex ($FilePath) {
         [eidsca] { $Category = 'EIDSCA'}
         [CISA] { $Category = 'CISA' }
         default { $Category = 'Core' }
     }
     $Status = 'Active'
-    $TagSort = @("$Category, $DirectoryName") | Sort-Object -Unique
-    $Tags = "$Status, $($TagSort.ToString())"
-    $Version = '0.0.1'
+    $Tags = "`'$Status`', `'$Category`'"
 
     $Info = @"
 <#PSScriptInfo
 .DESCRIPTION $Description
-.VERSION $Version
-.AUTHOR $Author
 .TAGS $Tags
+.AUTHOR The Maester Team
+.COMPANYNAME The Maester Team
+.COPYRIGHT Maester Team. All rights reserved.
+.VERSION '0.0.1'
+.GUID $Guid
+.ICONURI https://maester.dev/img/logo.svg
 #>
 `n
 "@
 
     $FileContent = Get-Content -Path $file
-    $StringBuilder = [System.Text.StringBuilder]::new(($FileContent) -join ([system.environment]::NewLine))
+    $StringBuilder = [System.Text.StringBuilder]::new(($FileContent) -join ([System.Environment]::NewLine))
     $null = $StringBuilder.Insert(0, $Info)
     $StringBuilder.ToString() | Set-Content -Path $FilePath -Encoding utf8
 }
