263a32e5758eb6ef9258e2f71922866dc3c0462a
Sam Erde <samuel.erde@gmail.com>
2025-09-22 08:12:19 -0400

    Fix typo in "Disconnnect."
    Fix non-standard spacing and indentation.

--- powershell/public/Disconnect-Maester.ps1
+++ powershell/public/Disconnect-Maester.ps1
@@ -1,48 +1,48 @@
-﻿<#
- .Synopsis
-    Helper method to sign out of the current Microsoft Graph session. Alternate for Disconnect-MgGraph.
+﻿function Disconnect-Maester {
+<#
+.Synopsis
+   Helper method to sign out of the current Microsoft Graph session. Alternate for Disconnect-MgGraph.
 
- .Description
-    Use this cmdlet to sign out of the current Microsoft Graph session.
+.Description
+   Use this cmdlet to sign out of the current Microsoft Graph session.
 
-    This cmdlet is a helper method for running the following command.
-    ```
-    Disconnnect-MgGraph
-    ```
+   This cmdlet is a helper method for running the following command.
+   ```
+   Disconnect-MgGraph
+   ```
 
- .Example
-    Disconnect-MtGraph
+.Example
+   Disconnect-MtGraph
 
- .Example
-    Disconnect-Maester
+.Example
+   Disconnect-Maester
 
- .Example
-    Disconnect-MtMaester
+.Example
+   Disconnect-MtMaester
 
 .LINK
-    https://maester.dev/docs/commands/Disconnect-Maester
+   https://maester.dev/docs/commands/Disconnect-Maester
 #>
-function Disconnect-Maester {
-   [Alias("Disconnect-MtMaester", "Disconnect-MtGraph")]
+   [Alias('Disconnect-MtMaester', 'Disconnect-MtGraph')]
    [CmdletBinding()]
    param()
 
-   if($__MtSession.Connections -contains "Graph" -or $__MtSession.Connections -contains "All"){
-      Write-Verbose -Message "Disconnecting from Microsoft Graph."
+   if ($__MtSession.Connections -contains 'Graph' -or $__MtSession.Connections -contains 'All') {
+      Write-Verbose -Message 'Disconnecting from Microsoft Graph.'
       Disconnect-MgGraph
    }
 
-   if($__MtSession.Connections -contains "Azure" -or $__MtSession.Connections -contains "All"){
-      Write-Verbose -Message "Disconnecting from Microsoft Azure."
+   if ($__MtSession.Connections -contains 'Azure' -or $__MtSession.Connections -contains 'All') {
+      Write-Verbose -Message 'Disconnecting from Microsoft Azure.'
       Disconnect-AzAccount
    }
 
-   if($__MtSession.Connections -contains "ExchangeOnline" -or $__MtSession.Connections -contains "SecurityCompliance" -or $__MtSession.Connections -contains "All"){
-      Write-Verbose -Message "Disconnecting from Microsoft Exchange Online."
+   if ($__MtSession.Connections -contains 'ExchangeOnline' -or $__MtSession.Connections -contains 'SecurityCompliance' -or $__MtSession.Connections -contains 'All') {
+      Write-Verbose -Message 'Disconnecting from Microsoft Exchange Online.'
       Disconnect-ExchangeOnline
    }
-   if($__MtSession.Connections -contains "Teams" -or $__MtSession.Connections -contains "All"){
-      Write-Verbose -Message "Disconnecting from Microsoft Teams."
+   if ($__MtSession.Connections -contains 'Teams' -or $__MtSession.Connections -contains 'All') {
+      Write-Verbose -Message 'Disconnecting from Microsoft Teams.'
       Disconnect-MicrosoftTeams
    }
 }
